3c1c3cfbd29b6efbd7401ddee1d272ad5ac3123d
albertopena123 <you@example.com>
2023-10-28 14:15:08 -0500

    hermano

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Alberto Peña MONDRAGON Next.js features and API.
+            Misael Peña MondragonNext.js features and API.
           </p>
         </a>
 
