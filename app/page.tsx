8fcf1227f9147e8ee48002679af5c7bde7aebe78
albertopena123 <you@example.com>
2023-10-29 11:47:06 -0500

    fondo
    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,42 +4,32 @@ export default function Home() {
   return (
    <main>
       <div className="bg-white">
-    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <h2 className="sr-only">Products</h2>
-  
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a href="#" className="group">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <Image src="https://tailwindui.com/Image/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
-          </div>
-          <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-          <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
-        </a>
-        <a href="#" className="group">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <Image src="https://tailwindui.com/Image/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
-          </div>
-          <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-          <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-        <a href="#" className="group">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <Image src="https://tailwindui.com/Image/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
-          </div>
-          <h3 className="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-          <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
-        </a>
-        <a href="#" className="group">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <Image src="https://tailwindui.com/Image/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
-          </div>
-          <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-          <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-  
+  <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+      <svg viewBox="0 0 1024 1024" className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
+        <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
+        <defs>
+          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+            <stop stop-color="#7775D6" />
+            <stop offset="1" stop-color="#E935C1" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boost your productivity.Start using our app today.</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+          <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+      <div className="relative mt-16 h-80 lg:mt-8">
+        <Image className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10" src="https://tailwindui.com/Image/component-images/dark-project-app-screenshot.png" alt="App screenshot" width="1824" height="1080"/>
       </div>
     </div>
   </div>
+</div>
+
    </main>
   
   
