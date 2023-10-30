5e1cfa465cd598498910db7c73c03e77a2a534bd
albertopena123 <you@example.com>
2023-10-29 21:24:38 -0500

    template deployed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '..public/style.css'
+import Head from 'next/head'
+import '..public/bundle.js'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en">Ç
+      <Head>
+      <meta charSet="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Base - Tailwind CSS Startup Template</title>
+      <link rel="icon" href="favicon.ico"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,36 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <main>
-      <div className="bg-white">
-  <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-      <svg viewBox="0 0 1024 1024" className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
-        <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
-        <defs>
-          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-            <stop stop-color="#7775D6" />
-            <stop offset="1" stop-color="#E935C1" />
-          </radialGradient>
-        </defs>
-      </svg>
-      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boost your productivity.Start using our app today.</h2>
-        <p className="mt-6 text-lg leading-8 text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
-        <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-          <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+      <section className="gj do ir hj sp jr i pg">
+        <div className="xc fn zd/2 2xl:ud-w-187.5 bd 2xl:ud-h-171.5 h q r">
+          <Image src="images/shape-01.svg" alt="shape" className="xc 2xl:ud-block h t -ud-left-[10%] ua" />
+          <Image src="images/shape-02.svg" alt="shape" className="xc 2xl:ud-block h u p va" />
+          <Image src="images/shape-03.svg" alt="shape" className="xc 2xl:ud-block h v w va" />
+          <Image src="images/shape-04.svg" alt="shape" className="h q r" />
+          <Image src="images/hero.png" alt="Woman" className="h q r ua" />
         </div>
-      </div>
-      <div className="relative mt-16 h-80 lg:mt-8">
-        <Image className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10" src="/images/poke.jpeg" alt="App screenshot" width="1824" height="1080"/>
-        
-      </div>
-    </div>
-  </div>
-</div>
+
+        <div className="bb ze ki xn 2xl:ud-px-0">
+          <div className="tc _o">
+            <div className="animate_left jn/2">
+              <h1 className="fk vj zp or kk wm wb">We specialize in UI/UX, Web Development, Digital Marketing.</h1>
+              <p className="fq">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla magna mauris. Nulla fermentum viverra sem eu rhoncus consequat varius nisi quis, posuere magna.
+              </p>
+
+              <div className="tc tf yo zf mb">
+                <a href="#" className="ek jk lk gh gi hi rg ml il vc _d _l"
+                  >Get Started Now </a>
+
+                <span className="tc sf">
+                  <a href="#" className="inline-block ek xj kk wm"> Call us (0123) 456 – 789 </a>
+                  <span className="inline-block">For any question or concern</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
    </main>
   
