a777222062cc63fb70850a92316aaa3d1943dc54
albertopena123 <you@example.com>
2023-10-29 22:20:45 -0500

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   <div className="bb ze ki xn 2xl:ud-px-0 oo wf yf i">
     <div className="vd to/4 tc wf yf">
       <a href="index.html">
-        <img className="om" src="images/trilce.png" alt="Logo Light" />
-        <img className="xc nm" src="images/logo-dark.svg" alt="Logo Dark" />
+        <Image className="om" src="images/trilce.png" alt="Logo Light" />
+        <Image className="xc nm" src="images/logo-dark.svg" alt="Logo Dark" />
       </a>
 
       <button className="po rc">
