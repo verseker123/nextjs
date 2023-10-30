918c34ff9c4b28eec2de25de994b8db64b12a39d
albertopena123 <you@example.com>
2023-10-29 21:33:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '..public/style.css'
+import './globals.css'
 import Head from 'next/head'
-import '..public/bundle.js'
 
 
 const inter = Inter({ subsets: ['latin'] })
